601e465616cb148cc889d9a7f01fd3e3b0c3380a
Caio Nask <caionask.dev@gmail.com>
2026-03-18 09:35:35 -0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions; 
+using System.Collections.Generic; // Para uso de listas.
+
+
+class Program
+{
+    static void Main()
+    {
+        Account account = new Account();
+
+        
+    }
+}
+
+/*
+
+Funcionalidades:
+
+criar conta
+depositar
+sacar
+mostrar saldo
+
+Conceitos:
+
+classes
+encapsulamento
+métodos
+validação
+
+*/
